95bafc3be6ea820d3285c5f09b1f0e14e827035c
Nesrine Nasri <nesrinasri99@gmail.com>
2026-03-04 10:30:21 +0100

    add register request method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -24,5 +26,30 @@ class AuthController extends Controller
     public function showRegister()
     {
         return view('Auth.Register');
+    }
+
+    /**
+     * Handle a registration request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        auth()->login($user);
+
+        return redirect('/')->with('success', 'Account created.');
     }
 }
