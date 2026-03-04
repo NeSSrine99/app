fae1d62246638c07ab63e065134c8a98853b3f38
Nesrine Nasri <nesrinasri99@gmail.com>
2026-03-04 10:25:41 +0100

    add show login/register in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    /**
+     * Show the login page.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function showLogin()
+    {
+        return view('Auth.Login');
+    }
+
+    /**
+     * Show the registration page.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function showRegister()
+    {
+        return view('Auth.Register');
+    }
 }
